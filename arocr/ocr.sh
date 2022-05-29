f212709e1ee0ea9c23f14626e4708db83520c3a1
mahsanghani <ghaniahsan@outlook.com>
2022-05-29 14:33:01 -0700

    shell script

---
+++ arocr/ocr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=0-2:59
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:8
+#SBATCH --mem-per-cpu=64G
+#SBATCH --account=def-mageed
+
+source ~/.bashrc
+source activate ENV39_hug
+
+################
+
+python3 /scratch/ahsang/dataset.py \
+      --dataset_name MADBase \
+      --train_split MADBase_Train \
+      --test_split MADBase_Test \
+      --root_dir ~/Documents/OCR/MADBase \
+      --save_dir processed/
+

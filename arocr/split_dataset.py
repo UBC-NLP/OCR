2627ded7126b3e2c22fbadc3ffb6e84307da5395
mahsanghani <ghaniahsan@outlook.com>
2022-05-30 12:59:30 -0700

    processing script

--- arocr/split_dataset.py
+++ arocr/split_dataset.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import train_test_split
 def split_dataset(datset, file):
     print ("loading the data")
     df = pd.read_csv(file, sep="\t")
+    df.rename(columns={'file_name': 'image'}, inplace=True)
+
     df_train, df_dev_test =  train_test_split(df, test_size= 0.2, random_state=41)
     df_test, df_dev =  train_test_split(df_dev_test, test_size= 0.5, random_state=41)
     print ("saving the split")
@@ -15,5 +17,5 @@ def split_dataset(datset, file):
 
 
 print ("start")
-dataset_name="OnlineKhatt"
-split_dataset(dataset_name, "./data/{}/{}.tsv".format(dataset_name,dataset_name))
+for dataset_name in ['OnlineKhatt', 'ADAB', 'IDPL-PFOD', 'shotor', 'UPTI']:
+    split_dataset(dataset_name, "./data/{}/{}.tsv".format(dataset_name,dataset_name))

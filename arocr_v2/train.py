f91fc7ced1be7a4680b96edb9d107f5668d0d710
Gagan Bhatia <gbhatia880@gmail.com>
2022-07-02 12:47:36 -0700

    v2

--- arocr_v2/train.py
+++ arocr_v2/train.py
@@ -45,7 +45,7 @@ def run(
         weight_decay=0.005,
         learning_rate=5e-5,
         seed=42,
-        report_to="wandb",
+        #report_to="wandb",
         load_best_model_at_end=True,
         metric_for_best_model="cer",
         greater_is_better=False,

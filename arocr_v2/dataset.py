ddd1a502e90307c543fbadd192a3dc6f06074dba
Gagan Bhatia <gbhatia880@gmail.com>
2022-07-02 12:56:34 -0700

    fix

--- arocr_v2/dataset.py
+++ arocr_v2/dataset.py
@@ -47,7 +47,7 @@ class OCRDataset(Dataset):
         labels = self.processor.tokenizer(text,
                                           padding="max_length",
                                           max_length=self.max_target_length,
-                                          truncation=True).input_ids.to(device)
+                                          truncation=True).input_ids
 
         labels = [label if label != self.processor.tokenizer.pad_token_id else -100 for label in labels]
 

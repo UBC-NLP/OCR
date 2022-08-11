4ceeeb14a54a9ef5783438a7e3c64548a20fb95e
mahsanghani <ghaniahsan@outlook.com>
2022-08-11 12:49:27 -0700

    train

---
+++ runs/train_tessract.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=10:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --account=def-mageed
+#SBATCH --job-name=train_tess
+#SBATCH --output=train_tess.out
+#SBATCH --error=train_tess.err
+#SBATCH --mail-user=gbhatia880@gmail.com
+#SBATCH --mail-type=ALL
+
+module load gcc/9.3.0 arrow python/3.9 scipy-stack tesseract cuda/11.4 opencv
+
+source ~/ENV38_default/bin/activate
+
+python tesseract.py 

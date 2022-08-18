7d1d68fb3defc0bfa34e8d5d17d6334b4b72a9dc
mahsanghani <ghaniahsan@outlook.com>
2022-08-18 11:40:26 -0700

    all

--- runs/train_all.sh
+++ runs/train_all.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-#SBATCH --account=def-mageed
-#SBATCH --gres=gpu:v100l:4
+#SBATCH --time=3:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:1
+#SBATCH --account=rrg-mageed
+#SBATCH --job-name=train_hf_v2
+#SBATCH --output=train_hf_v2.out
+#SBATCH --error=train_hf_v2.err
 #SBATCH --mail-user=gbhatia880@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH --nodes=1
-#SBATCH --mem=64G
-#SBATCH --time=10:00:00
-#SBATCH --job-name=run_all_models_v2
-#SBATCH --output=out_%x.out
-#SBATCH --error=err_%x.err
 
 module load python/3.8
 module load scipy-stack
@@ -26,6 +28,6 @@ pwd
 
 echo "Training started at $(date)"
 
-wandb agent gagan3012/arocr/v5etz5et
+python train_all.py
 
 echo "Training ended at $(date)"
